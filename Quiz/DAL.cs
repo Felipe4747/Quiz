e465bb4136e2ed7a77bf0e2083909c8ee8c820dd
Eduardokpv <epontesveiga@hotmail.com>
2018-10-04 11:15:24 -0300

    conectando bunitinhu

---
+++ Quiz/DAL.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Quiz
+{
+    class DAL
+    {
+        #region Atributos
+        private MySqlConnection _conexao;
+        private MySqlCommand _comandoDB;
+        private MySqlDataAdapter _adaptador;
+        private DataSet _dados;
+        #endregion
+
+        #region Getters e Setters 
+        public string Host { get; set; } = "localhost";
+        public string DBNome { get; set; } = "quiz";
+        public string Usuario { get; set; } = "root";
+        public string Senha { get; set; } = "";
+        public string Pergunta { get; set; } = "";
+        #endregion
+
+        #region Construtor
+        public DAL() { }
+        #endregion
+
+        #region Métodos
+        public void Conectar()
+        {
+            string strConexao = "Server=" + Host;
+            strConexao += ";Database=" + DBNome;
+            strConexao += ";Uid=" + Usuario;
+            strConexao += ";Pwd=" + Senha;
+            strConexao += ";SSL Mode=None";
+            try
+            {      
+                _conexao = new MySqlConnection(strConexao);
+                if (_conexao.State.Equals(ConnectionState.Closed))
+                {
+                    _conexao.Open();
+                }
+
+            }
+            catch (MySqlException erro)
+            {
+                throw new Exception(erro.Message);
+            }
+            finally
+            {
+                _conexao.Dispose();
+            }
+        }
+ 
+       public void Inserir(string strSQL)
+        {
+            try
+            {
+                if (_conexao.State.Equals(ConnectionState.Closed))
+                {
+                    _conexao.Open();
+                }
+
+                _comandoDB = _conexao.CreateCommand();
+                _comandoDB.CommandText = strSQL;
+
+                if (_comandoDB.ExecuteNonQuery() != 1)
+                {
+                    throw new Exception("Falha ao inserir");
+                }
+            }
+            catch (MySqlException erro)
+            {
+                throw new Exception("Erro ao inserir: " + erro.Message);
+            }
+        }
+        public DataSet Buscar(string strSQL)
+        {
+            _dados = new DataSet();
+            _adaptador = new MySqlDataAdapter(strSQL, _conexao);
+            _adaptador.Fill(_dados, "tbl_resultado");
+            return _dados;
+        }
+        public void Atualizar() { }
+        public void Excluir() { }
+        #endregion
+    }
+}
--- Quiz/Main.cs
+++ Quiz/Main.cs
@@ -12,12 +12,19 @@ namespace Quiz
 {
     public partial class Main : Form
     {
+        private DAL _banco = new DAL();
         int pontos = 0;
         int cont = 20;
+        List<DataSet> perguntas = new List<DataSet>();
         public Main()
         {
             InitializeComponent();
-            
+            _banco.DBNome = "quiz";
+            _banco.Conectar();
+            for (int i = 1; i < 31; i++)
+            {
+                perguntas.Add(_banco.Buscar("select pergunta from perguntas where id = " + i.ToString() + ";"));
+            }
         }
 
         private void tempo_Tick(object sender, EventArgs e)
@@ -35,6 +42,11 @@ namespace Quiz
         {
             //Fazer as coisas pra mudar a pergunta
             cont = 20;
+            Random rdn = new Random();
+            int num;
+            num = rdn.Next(1, 30);
+            pergunta.Text = Convert.ToString(perguntas[num]);
+            perguntas.Remove(perguntas[num]);
         }
         #endregion
     }
