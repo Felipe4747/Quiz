f60d470dfe1efae46d6290b623f2b969e59c5763
Eduardokpv <epontesveiga@hotmail.com>
2018-10-05 09:13:34 -0300

    tentativa de mudar botão

--- Quiz/Main.Designer.cs
+++ Quiz/Main.Designer.cs
@@ -53,6 +53,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -65,6 +66,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -77,6 +79,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -89,6 +92,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label1
             // 
--- Quiz/Main.cs
+++ Quiz/Main.cs
@@ -15,10 +15,18 @@ namespace Quiz
         private DAL _banco = new DAL();
         int pontos = 0;
         int cont = 5;
+        string respCerta;
         List<string> perguntas = new List<string>();
+        DataSet respostasDataset;
+        List<string> respostas = new List<string>();
+        
         public Main()
         {
             InitializeComponent();
+            respostas.Add("resp1");
+            respostas.Add("resp2");
+            respostas.Add("resp3");
+            respostas.Add("resp4");
             _banco.DBNome = "quiz";
             _banco.Conectar();
             DataSet perguntasDataset = _banco.Buscar("select pergunta from perguntas;");
@@ -48,7 +56,53 @@ namespace Quiz
             num = rdn.Next(1, perguntas.Count);
             pergunta.Text = Convert.ToString(perguntas[num]);
             perguntas.Remove(perguntas[num]);
+            DataSet respDataset = _banco.Buscar("select resp_certa from perguntas where id = " + num + ";");
+            respCerta = respDataset.Tables["tbl_resultado"].Rows[num]["resp_certa"].ToString();
+            respostasDataset = _banco.Buscar("select " + respostas[3] + " from perguntas where id = " + num + ";");
+            button1.Text = respostasDataset.Tables["tbl_resultado"].Rows[num][respostas[3]].ToString();
+            respostasDataset = _banco.Buscar("select " + respostas[4] + " from perguntas where id = " + num + ";");
+            button2.Text = respostasDataset.Tables["tbl_resultado"].Rows[num][respostas[4]].ToString();
+            respostasDataset = _banco.Buscar("select " + respostas[5] + " from perguntas where id = " + num + ";");
+            button3.Text = respostasDataset.Tables["tbl_resultado"].Rows[num][respostas[5]].ToString();
+            respostasDataset = _banco.Buscar("select " + respostas[6] + " from perguntas where id = " + num + ";");
+            button4.Text = respostasDataset.Tables["tbl_resultado"].Rows[num][respostas[6]].ToString();
         }
         #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (respCerta == "a")
+            {
+                pontos++;
+            }
+            MudaPergunta();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (respCerta == "b")
+            {
+                pontos++;
+            }
+            MudaPergunta();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (respCerta == "c")
+            {
+                pontos++;
+            }
+            MudaPergunta();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (respCerta == "d")
+            {
+                pontos++;
+            }
+            MudaPergunta();
+        }
     }
 }
