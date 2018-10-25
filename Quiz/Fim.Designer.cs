49def1e5551c73ba84296e588bcc2b0026480e40
Eduardokpv <36730990+Eduardokpv@users.noreply.github.com>
2018-10-25 10:44:22 -0200

    ola

--- Quiz/Fim.Designer.cs
+++ Quiz/Fim.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(1184, 682);
             this.Name = "Fim";
             this.Text = "Fim";
