cb81ea4be61626c345ad07a5844dd5bddd4d5320
Eduardokpv <36730990+Eduardokpv@users.noreply.github.com>
2018-10-25 10:44:39 -0200

    oi

--- Quiz/RegistroUsuario.Designer.cs
+++ Quiz/RegistroUsuario.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // RegistroUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1184, 682);
+            this.Name = "RegistroUsuario";
             this.Text = "RegistroUsuario";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
